d60e1a70fcb20b099c329147482ea382fb6b05f4
Lhure <maxjonilsson@gmail.com>
2018-03-27 12:05:16 +0200

    to remove

--- OldGame/Object.cpp
+++ OldGame/Object.cpp
@@ -57,4 +57,5 @@ void Object::createconstBuffData()
 {
 	Locator::getD3D()->createConstantBuffer(&this->constBuff, sizeof(objectBuff));
 	int hej = 0; //https://github.com/feliciasp/LitetSpel_TA_GPR3.git
+	// Lhure e dålig
 }
